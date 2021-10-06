020ea82717197f83c142f1ff90930299608d2235
Neesky <945823487@qq.com>
2021-10-07 02:13:38 +0800

    删除一些没有用的东西

--- Vivado/Lab_04.ip_user_files/ip/inst_ram/inst_ram_stub.vhdl
+++ Vivado/Lab_04.ip_user_files/ip/inst_ram/inst_ram_stub.vhdl
@@ -1,34 +1,34 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
--- Date        : Thu Oct  7 01:18:24 2021
+-- Date        : Thu Oct  7 01:19:46 2021
 -- Host        : DESKTOP-L0IVESM running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
---               C:/Users/94582/Documents/GitHub/CPU/Vivado/Lab_04.srcs/sources_1/ip/data_ram/data_ram_stub.vhdl
--- Design      : data_ram
+--               C:/Users/94582/Documents/GitHub/CPU/Vivado/Lab_04.srcs/sources_1/ip/inst_ram/inst_ram_stub.vhdl
+-- Design      : inst_ram
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7a35tcsg325-1
 -- --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity data_ram is
+entity inst_ram is
   Port ( 
     clka : in STD_LOGIC;
     ena : in STD_LOGIC;
     wea : in STD_LOGIC_VECTOR ( 0 to 0 );
-    addra : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 7 downto 0 );
     dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
     douta : out STD_LOGIC_VECTOR ( 31 downto 0 )
   );
 
-end data_ram;
+end inst_ram;
 
-architecture stub of data_ram is
+architecture stub of inst_ram is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[9:0],dina[31:0],douta[31:0]";
+attribute black_box_pad_pin of stub : architecture is "clka,ena,wea[0:0],addra[7:0],dina[31:0],douta[31:0]";
 attribute x_core_info : string;
 attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1";
 begin
--- Vivado/Lab_04.ip_user_files/sim_scripts/data_ram/activehdl/compile.do
+++ Vivado/Lab_04.ip_user_files/sim_scripts/data_ram/activehdl/compile.do
@@ -10,16 +10,16 @@ vmap xpm activehdl/xpm
 vmap blk_mem_gen_v8_4_3 activehdl/blk_mem_gen_v8_4_3
 
 vlog -work xil_defaultlib  -sv2k12 \
-"E:/xilinx/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+"C:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
 
 vcom -work xpm -93 \
-"E:/xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
+"C:/Xilinx/Vivado/2019.1/data/ip/xpm/xpm_VCOMP.vhd" \
 
 vlog -work blk_mem_gen_v8_4_3  -v2k5 \
 "../../../ipstatic/simulation/blk_mem_gen_v8_4.v" \
 
 vlog -work xil_defaultlib  -v2k5 \
-"../../../../Lab_03.srcs/sources_1/ip/data_ram/sim/data_ram.v" \
+"../../../../Lab_04.srcs/sources_1/ip/data_ram/sim/data_ram.v" \
 
 
 vlog -work xil_defaultlib \
